33233a90389778277dc8839cf82fc9caaca4dfa9
arturfrut <arturfruttero@gmail.com>
2023-11-18 16:34:36 -0300

    FreeEventDay align fixed

--- src/app/(with-nav-bar)/clasesymilongas/FreeEventDay.tsx
+++ src/app/(with-nav-bar)/clasesymilongas/FreeEventDay.tsx
@@ -1,9 +1,9 @@
-import { Card, CardBody, Image } from '@nextui-org/react'
+import { Card, CardBody, Image } from '@nextui-org/react';
 
 export default function FreeEventDay() {
   return (
     <Card>
-      <CardBody className='flex gap-3'>
+      <CardBody className='flex flex-row items-center gap-3'>
         <Image
           alt='nextui logo'
           height={100}
@@ -11,8 +11,8 @@ export default function FreeEventDay() {
           src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvboFcgiIgfkVkbHJu5rWGdffSaEsCZRbXcbULPRhzD_xEkVWT_DN3TyKUz123_2XTOCc&usqp=CAU'
           width={100}
         />
-          <p className='text-md'>Lo sentimos, pero no hay eventos en esta fecha</p>
+        <p className='text-md'>Lo sentimos, pero no hay eventos en esta fecha</p>
       </CardBody>
     </Card>
-  )
+  );
 }
