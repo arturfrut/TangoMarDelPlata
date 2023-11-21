94a45dbe900784b09ad123fb2b212acab33b0d75
arturfrut <arturfruttero@gmail.com>
2023-11-21 00:22:11 -0300

    work in classes

--- src/app/(with-nav-bar)/clasesymilongas/EventItem.tsx
+++ src/app/(with-nav-bar)/clasesymilongas/EventItem.tsx
@@ -1,7 +1,15 @@
 'use client'
 
 import { Event } from '@/types/eventType'
-import { Card, CardHeader, CardBody, CardFooter, Divider, Link, Image, Button } from '@nextui-org/react'
+import {
+  Button,
+  Card,
+  CardFooter,
+  CardHeader,
+  Divider,
+  Image,
+  Link
+} from '@nextui-org/react'
 //TO DO
 // Si no hay imagen cargar fondo negro con iniciales
 // Pensar campos
@@ -11,33 +19,55 @@ import { Card, CardHeader, CardBody, CardFooter, Divider, Link, Image, Button } 
 // Iconos eventos
 // Función para saber a q hora es el evento
 
-export default function EventItem({ event }:{event: Event}) {
+export default function EventItem({ event }: { event: Event }) {
   return (
     <Card>
-      <CardHeader className='flex gap-3'>
-        <Image alt='nextui logo' height={100} radius='sm' src={event.img} width={100} />
-        <div className='w-1/2 p-4'>
-          <p className='text-md'>{event.eventName}</p>
-          { // AGREGAR VALIDACIÓN PARA SABER CUANTO FALTA
-          false ? (
-            <p className='text-md'>
-              <span className='text-md animate-pulse text-green-600'>{'\u2022'}</span> En curso
+      <CardHeader className='flex gap-3 justify-between'>
+        <div className='flex gap-3 items-center '>
+          <Image
+            alt='nextui logo'
+            height={100}
+            radius='sm'
+            src={event.img}
+            width={100}
+          />
+          <div className=' p-4'>
+            <p className='text-md'>{event.eventName}</p>
+            {
+              // AGREGAR VALIDACIÓN PARA SABER CUANTO FALTA
+              false ? (
+                <p className='text-md'>
+                  <span className='text-md animate-pulse text-green-600'>
+                    {'\u2022'}
+                  </span>{' '}
+                  En curso
+                </p>
+              ) : (
+                <p className='text-md'>
+                  <span className='text-md animate-pulse text-red-600'>
+                    {'\u2022'}
+                  </span>{' '}
+                  Próxima clase en XXX horas
+                </p>
+              )
+            }
+            <p className='text-small text-default-500'>{event.address}</p>
+            <p className='text-small text-default-500'>
+              Organizan:{' '}
+              {event.organizers.map((organizer, i) => (
+                <span>
+                  {organizer.alias}{' '}
+                  {event.organizers.length > 1 &&
+                    i < event.organizers.length - 2 &&
+                    ', '}
+                  {event.organizers.length > 1 &&
+                    i === event.organizers.length - 2 &&
+                    ' y '}
+                </span>
+              ))}
             </p>
-          ) : (
-            <p className='text-md'>
-              <span className='text-md animate-pulse text-red-600'>{'\u2022'}</span> Próxima clase en XXX horas
-            </p>
-          )}
-          <p className='text-small text-default-500'>{event.address}</p>
-          <p className='text-small text-default-500'>
-            Organizan:{' '}
-            {event.organizers.map((organizer, i) => (
-              <span>
-                {organizer.alias} {event.organizers.length < i && 'y '}
-              </span>
-            ))}
-          </p>
-          <p className='text-small text-default-500'>{event.schedule}</p>
+            <p className='text-small text-default-500'>{event.schedule}</p>
+          </div>
         </div>
         <div
           className='w-1/8 p-2
